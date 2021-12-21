4714c0a563b786f4f1f1417885d7ed27ca17c225
Elier H <elierporto@gmail.com>
2021-12-21 17:59:38 -0500

    Add: react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import Home from "./pages/Home";
+import Index from "./pages";
 import { DarkModeProvider } from "./context/DarkModeProvider";
+import { Routes, Route } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import Theme from "./layout/Theme";
+import { ContentDataProvider } from "./context/ContentDataProvider";
 
 function App() {
   return (
     <DarkModeProvider>
-      <Home />
+      <Theme>
+        <CssBaseline>
+          <ContentDataProvider>
+            <Routes>
+              <Route path="/" element={<Index />} />
+            </Routes>
+          </ContentDataProvider>
+        </CssBaseline>
+      </Theme>
     </DarkModeProvider>
   );
 }
