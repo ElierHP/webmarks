eb9883172375de685bd4a9e1c244b81930c5b1a9
Elier H <elierporto@gmail.com>
2022-11-28 14:03:41 -0500

    Fix: server url

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const baseUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000"
-    : "https://webmarks-server-production.up.railway.app/";
+    : "https://webmarks-server-production.up.railway.app";
 
 export const deleteItem = async (query, _id, dispatch) => {
   const res = await axios.delete(`${baseUrl}/${query}/delete`, {
